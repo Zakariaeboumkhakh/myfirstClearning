0347f9f3078cbb9e79fb1d43d493c2939be08fea
echarradahmed <echarradahmed@gmail.com>
2023-06-04 05:25:28 -0700

    good news

---
+++ Alx_projects/0x00-hello_world/101-quote.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+
---
+++ Alx_projects/0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int N;
+	char ch;
+	long n1;
+	long long int n2;
+	float N3;
+	printf("the size of char is variable %lu byte(s)\n", sizeof(char));
+	printf("the size of int variable is %lu byte(s) \n", sizeof(int));
+	printf("the size of long variable is %lu byte(s) \n", sizeof(long));
+	printf("the size of long long int variable is %lu byte(s) \n",sizeof(long long int));
+	printf("the size of float variale is %lu byte(s) \n", sizeof(float));
+	return (0);
+}
